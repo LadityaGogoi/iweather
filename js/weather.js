780dcccf993147243ccf6bcc564de2479e5787f2
Laditya_29 <108052802+dr-jackaal@users.noreply.github.com>
2023-02-06 19:43:24 +0530

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -7,70 +7,91 @@ const timeOutput = document.querySelector('.time');
 const conditionOutput = document.querySelector('.condition');
 const nameOutput = document.querySelector('.name');
 const icon = document.querySelector('.icon');
-const cloudOutput = document.querySelector('.cloud');
+
+const cloudOutput = document.querySelector('cloud');
 const humidityOutput = document.querySelector('.humidity');
 const windOutput = document.querySelector('.wind');
-const form = document.querySelector('locationInput');
+
+const form = document.querySelector('#locationInput');
 const search = document.querySelector('.search');
 const btn = document.querySelector('.submit');
 const cities = document.querySelectorAll('.city');
 
+
+
 let cityInput = "london";
 
-
-cities.forEach((city)=>{
-    city.addEventListener('click', (e)=>{
-        // change from the default city to the click one
-        // console.log(e.target.innerHTML);
+cities.forEach((city) => {
+    city.addEventListener('click', (e) => {
         cityInput = e.target.innerHTML;
+        console.log(cityInput);
 
+    // app.style.opacity = "0";
         fetchWeatherData();
-
-        app.style.opacity = "0";
     });
 });
 
 
-// Add submit event to the form
-form.addEventListener('submit', (e)=>{
 
-    if (search.value.length == 0)
-    {
-        alert("Please! type a city name.");
-    }
-    else 
-    {
-        cityInput = search.value;
-
-        fetchWeatherData();
-
-        search.value = "";
-
-        app.style.opacity = "0"
-    }
-    // Prevent default behaviour of the form 
-    e.preventDefault();
-});
-
-function dayOfTheWeek(day, month, year) {
-    const weekday = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thrusday",
-        "Friday",
-        "Saturday"
-    ];
-    return weekday[new Date('${day}/${month}/${year}').getDay()];
-};
-
+// Function that fatches and displays the data from the weather API
 
 function fetchWeatherData() {
-    fetch('http://api.weatherapi.com/v1/current.json?key=c5c4660114d2479890a111029232501&q=${cityInput}')
+    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} 7040acf1b2ae3a14b95a946862a11d1b
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=7040acf1b2ae3a14b95a946862a11d1b')
+    
 
+    // Fetch the data (which is in JSON format) adn convert it to a regular JS object
     .then(response => response.json())
     .then(data => {
         console.log(data);
+
+        temp.innerHTML = data.main.temp + "° Celcius";
+        conditionOutput.innerHTML = data.weather[0].main;
+
+        // Get the date and time from the citya and extract the day, month, year and time into individual
+
+
+
+
+
+
+        // add the name of the city into the page
+        nameOutput.innerHTML = cityInput;
+
+
+        // get the corresponding icon url for the weather and extract a part of it
+        const iconId = data.weather[0].icon;
+        icon.src = "http://openweathermap.org/img/wn/" + iconId + "@2x.png";
+
+        const ID = data.weather[0].id;
+        if (
+            ID === 200 ||
+            ID === 201 ||
+            ID === 202 ||
+            ID === 210 ||
+            ID === 211 ||
+            ID === 212 ||
+            ID === 221 ||
+            ID === 230 ||
+            ID === 231 ||
+            ID === 232
+        ) {
+            app.style.backgroundImage = 'url(/images/day/rainy.jpg';
+        }
+        else if (
+            ID === 801 ||
+            ID === 801 ||
+            ID === 803 ||
+            ID === 804
+        ) {
+            app.style.backgroundImage = 'url(/images/day/rainy.jpg';
+        } 
+
+
     })
 }
+
+
+
+
+
