f7335f868a0331da2c8af0694b47c1f9ec5f4da8
Laditya_29 <108052802+dr-jackaal@users.noreply.github.com>
2023-02-07 17:19:06 +0530

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -8,11 +8,11 @@ const conditionOutput = document.querySelector('.condition');
 const nameOutput = document.querySelector('.name');
 const icon = document.querySelector('.icon');
 
-const cloudOutput = document.querySelector('cloud');
+const cloudOutput = document.querySelector('.cloud');
 const humidityOutput = document.querySelector('.humidity');
 const windOutput = document.querySelector('.wind');
 
-const form = document.querySelector('#locationInput');
+const form = document.getElementById('form');
 const search = document.querySelector('.search');
 const btn = document.querySelector('.submit');
 const cities = document.querySelectorAll('.city');
@@ -20,78 +20,187 @@ const cities = document.querySelectorAll('.city');
 
 
 let cityInput = "london";
+fetchWeatherData();
+
+
 
 cities.forEach((city) => {
     city.addEventListener('click', (e) => {
         cityInput = e.target.innerHTML;
+
         console.log(cityInput);
 
-    // app.style.opacity = "0";
         fetchWeatherData();
-    });
+    })
 });
 
 
 
-// Function that fatches and displays the data from the weather API
-
-function fetchWeatherData() {
-    // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} 7040acf1b2ae3a14b95a946862a11d1b
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityInput + '&appid=7040acf1b2ae3a14b95a946862a11d1b')
-    
-
-    // Fetch the data (which is in JSON format) adn convert it to a regular JS object
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-
-        temp.innerHTML = data.main.temp + "° Celcius";
-        conditionOutput.innerHTML = data.weather[0].main;
-
-        // Get the date and time from the citya and extract the day, month, year and time into individual
 
 
 
 
 
 
-        // add the name of the city into the page
-        nameOutput.innerHTML = cityInput;
-
-
-        // get the corresponding icon url for the weather and extract a part of it
-        const iconId = data.weather[0].icon;
-        icon.src = "http://openweathermap.org/img/wn/" + iconId + "@2x.png";
-
-        const ID = data.weather[0].id;
-        if (
-            ID === 200 ||
-            ID === 201 ||
-            ID === 202 ||
-            ID === 210 ||
-            ID === 211 ||
-            ID === 212 ||
-            ID === 221 ||
-            ID === 230 ||
-            ID === 231 ||
-            ID === 232
-        ) {
-            app.style.backgroundImage = 'url(/images/day/rainy.jpg';
-        }
-        else if (
-            ID === 801 ||
-            ID === 801 ||
-            ID === 803 ||
-            ID === 804
-        ) {
-            app.style.backgroundImage = 'url(/images/day/rainy.jpg';
-        } 
-
-
-    })
+// function for click event
+function formSubmit(event) {
+    console.log(event);
 }
 
+// Submit event to the list
+form.addEventListener('submit', formSubmit);
 
 
 
 
+
+
+
+
+
+
+
+// function to find the day of the week
+function dayOfTheWeek(day, month, year) {
+    const weekday = [
+        "Friday",
+        "Saturday",
+        "Sunday",
+        "Monday",
+        "Tuesday",        
+        "Wednesday",
+        "Thrusday",
+    ];
+    return weekday[new Date(day/month/year).getDay()];
+};
+
+
+
+// fetching the data for the website
+function fetchWeatherData() {
+    fetch('http://api.weatherapi.com/v1/current.json?key=2187b62e323e464b9b9195238230602&q=' + cityInput + '&aqi=no')
+
+
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+
+        temp.innerHTML = data.current.temp_c + "&#176;" + " celcius"; 
+        conditionOutput.innerHTML = data.current.condition.text;
+
+
+        // Get the date and time for that place
+        const date = data.location.localtime;
+        const y = parseInt(date.substr(0, 4));
+        const m = parseInt(date.substr(5, 2));
+        const d = parseInt(date.substr(8, 2));
+        const time = date.substr(11);
+        // console.log(y, m, d);
+
+
+        timeOutput.innerHTML = time;
+        const dayWeek = dayOfTheWeek(d, m, y);
+        dateOutput.innerHTML = dayWeek + " " + m + "/" + y;
+
+
+
+        nameOutput.innerHTML = data.location.name;
+
+
+
+        // getting icon 
+        icon.src = data.current.condition.icon;
+
+
+
+        // adding the weather details
+        cloudOutput.innerHTML = data.current.cloud + "%";
+        humidityOutput.innerHTML = data.current.humidity + "%";
+        windOutput.innerHTML = data.current.wind_kph + "km/h";
+
+
+        // set the default time of day
+        let timeOfDay = "day";
+        if (!data.current.is_day)
+        {
+            timeOfDay = "night";
+        }
+
+        // console.log(timeOfDay);
+        const code = data.current.condition.code;
+        if (code === 1000)
+        {
+            // Set the background to clear if the weather is clear
+            app.style.backgroundImage = 'url(/iWeather/images/' + timeOfDay + '/clear.jpg)';
+
+
+            // change the button bg color depending on if its day or night
+            btn.style.background = "#e5ba92";
+            if (timeOfDay == "night")
+            {
+                btn.style.background = "#181e27";
+            }
+        }
+        else if (
+            code === 1003 ||
+            code === 1006 ||
+            code === 1009 ||
+            code === 1030 ||
+            code === 1069 ||
+            code === 1087 ||
+            code === 1135 ||
+            code === 1273 ||
+            code === 1276 ||
+            code === 1279 ||
+            code === 1282
+        ) {
+            // cloudy weather
+            app.style.backgroundImage = 'url(/iWeather/images/' + timeOfDay + '/cloudy.jpg)';
+
+            btn.style.background = "#fa6d1b";
+            if (timeOfDay == "night")
+            {
+                btn.style.background = "#181e27";
+            }
+        }
+        else if (
+            code === 1063 ||
+            code === 1069 ||
+            code === 1072 ||
+            code === 1150 ||
+            code === 1153 ||
+            code === 1180 ||
+            code === 1183 ||
+            code === 1186 ||
+            code === 1189 ||
+            code === 1192 ||
+            code === 1195 ||
+            code === 1204 ||
+            code === 1207 ||
+            code === 1240 ||
+            code === 1243 ||
+            code === 1246 ||
+            code === 1249 ||
+            code === 1252
+        ) {
+            // rainy weather
+            app.style.backgroundImage = 'url(/iWeather/images/' + timeOfDay + '/cloudy.jpg)';
+
+            btn.style.background = "#647d75";
+            if (timeOfDay === "night") 
+            {
+                btn.style.background = "#325c80";
+            }
+        }
+        else {
+            // snowy weather
+            app.style.backgroundImage = 'url(/iWeather/images/' + timeOfDay + '/snowy.jpg)';
+
+            btn.style.background = "#4d72aa";
+            if (timeOfDay === "night")
+            {
+                btn.style.background = "#1b1b1b";
+            }
+        }
+    })
+};
